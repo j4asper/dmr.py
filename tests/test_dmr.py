43d7ece354a65692aa196d44d4db9c0af015337b
j4asper <jasper@jazper.dk>
2022-07-23 21:50:36 +0200

    Add  more testing stuff

--- tests/test_dmr.py
+++ tests/test_dmr.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from dmr import DMR
 
 def test_get_by_plate():
@@ -5,3 +6,5 @@ def test_get_by_plate():
     dmr_obj = DMR().get_by_plate(license_plate="cw87553")
     
     assert dmr_obj.make == "Suzuki"
+    assert dmr_obj.first_registration == datetime(2005, 8, 22, 0, 0)
+    assert dmr_obj.vin == "TSMMZC21S00122899"
