3d48e6675c1c6e2bff6c60477d3dcce5ad7a5f87
j4asper <jasper@jazper.dk>
2022-07-24 15:54:49 +0200

    Add data type changing

--- dmr/utils/clean_data.py
+++ dmr/utils/clean_data.py
@@ -2,14 +2,23 @@ from datetime import datetime
 
 def clean(data):
     for key, value in data.items():
-        if value.startswith("-"):
+        if value.startswith("-") or value == "Ukendt":
             data[key] = None
-        if value == "Nej":
-            data[key] = False
-        if value == "Ja":
-            data[key] = True
-        if value == "Ukendt":
-            data[key] = None
+            continue
+        if value == "Nej" or value == "Ja":
+            data[key] = True if value == "Ja" else False
+            continue
         if key == "last_update" or key == "first_registration":
             data[key] = datetime.strptime(value, "%d-%m-%Y")
+            continue
+        if  any(key==ckey for ckey in ["fuel_consumption", "electricity_consumption", "battery_capacity", "electric_range"]):
+            value = value.replace(",", ".")
+            try:
+                value = float(value)
+                data[key] = float(value)
+                continue
+            except ValueError:
+                pass
+        if value.isnumeric():
+            data[key] = int(value)
     return data
