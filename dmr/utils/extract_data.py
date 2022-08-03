1155d8f197ee896281ad53843ea6e95a1d866cab
j4asper <jasper@jazper.dk>
2022-08-03 14:46:12 +0200

    Use xpaths from xpaths.py

--- dmr/utils/extract_data.py
+++ dmr/utils/extract_data.py
@@ -1,36 +1,43 @@
+from .xpaths import XPATHS
+
+def get_value_from_xpath(source, xpath_string):
+    return source.xpath(xpath_string)[0].text_content()
+
 # Landing page when looking up a licens plate, the page title is "1. Køretøj"
 def page_1(source):
     data = dict()
-    make, model, variant = source.xpath('/html/body/div[2]/div/div[1]/div[2]/div[3]/div/div[2]/div[1]/div[2]/div/div[1]/div[1]/div/div[2]/span[2]')[0].text_content().split(",", 2)
-    data["make"], data["model"], data["variant"] = make.strip().lower().capitalize(), model.strip().lower().capitalize(), variant.strip().lower().capitalize()
-    data["vin"] = source.xpath('/html/body/div[2]/div/div[1]/div[2]/div[3]/div/div[2]/div[1]/div[2]/div/div[1]/div[1]/div/div[1]/span[2]')[0].text_content()
-    data["type"] = source.xpath('/html/body/div[2]/div/div[1]/div[2]/div[3]/div/div[2]/div[1]/div[2]/div/div[1]/div[1]/div/div[3]/span[2]')[0].text_content()
-    data["last_update"] = source.xpath('/html/body/div[2]/div/div[1]/div[2]/div[3]/div/div[2]/div[1]/div[2]/div/div[1]/div[1]/div/div[4]/span[2]')[0].text_content().split(" ")[-1]
-    data["registration_number"] = source.xpath('/html/body/div[2]/div/div[1]/div[2]/div[3]/div/div[2]/div[1]/div[2]/div/div[1]/div[2]/div/div[1]/span[2]')[0].text_content()
-    data["first_registration"] = source.xpath('/html/body/div[2]/div/div[1]/div[2]/div[3]/div/div[2]/div[1]/div[4]/div/div[2]/div[1]/div[2]/span')[0].text_content()
-    data["use"] = source.xpath('/html/body/div[2]/div/div[1]/div[2]/div[3]/div/div[2]/div[1]/div[2]/div/div[1]/div[2]/div/div[3]/span[2]')[0].text_content()
-    data["vehicle_id"] = source.xpath('/html/body/div[2]/div/div[1]/div[2]/div[3]/div/div[2]/div[1]/div[4]/div/div[1]/div[1]/div[2]/span')[0].text_content()
-    data["color"] = source.xpath('/html/body/div[2]/div/div[1]/div[2]/div[3]/div/div[2]/div[1]/div[4]/div/div[1]/div[3]/div[2]/span')[0].text_content()
-    data["model_year"] = source.xpath('/html/body/div[2]/div/div[1]/div[2]/div[3]/div/div[2]/div[1]/div[4]/div/div[1]/div[4]/div[2]/span')[0].text_content()
-    # data[""] = source.xpath('')[0].text_content()
+    make, model, variant = get_value_from_xpath(source, XPATHS["page_1"]["make_model_variant"]).split(",", 2)
+    # Make make/model/variant names prettier
+    data["make"] = make.strip().capitalize()
+    data["model"] = model.strip().capitalize()
+    data["variant"] = variant.strip().capitalize()
+    data["vin"] = get_value_from_xpath(source, XPATHS["page_1"]["vin"])
+    data["type"] = get_value_from_xpath(source, XPATHS["page_1"]["type"])
+    data["last_update"] = get_value_from_xpath(source, XPATHS["page_1"]["last_update"]).split(" ")[-1]
+    data["registration_number"] = get_value_from_xpath(source, XPATHS["page_1"]["registration_number"])
+    data["first_registration"] = get_value_from_xpath(source, XPATHS["page_1"]["first_registration"])
+    data["use"] = get_value_from_xpath(source, XPATHS["page_1"]["use"])
+    data["vehicle_id"] = get_value_from_xpath(source, XPATHS["page_1"]["vehicle_id"])
+    data["color"] = get_value_from_xpath(source, XPATHS["page_1"]["color"])
+    data["model_year"] = get_value_from_xpath(source, XPATHS["page_1"]["model_year"])
     return data
 
 # page title is "2. Tekniske oplysninger"
 def page_2(source):
     data = dict()
-    data["total_weight"] = source.xpath('/html/body/div[2]/div/div[1]/div[2]/div[3]/div/div[2]/div[1]/div[2]/div/div[1]/div[2]/div[2]/span[1]')[0].text_content()
-    data["vehicle_weight"] = source.xpath('/html/body/div[2]/div/div[1]/div[2]/div[3]/div/div[2]/div[1]/div[2]/div/div[1]/div[3]/div[2]/span[1]')[0].text_content()
-    data["propulsion"] = source.xpath('/html/body/div[2]/div/div[1]/div[2]/div[3]/div/div[2]/div[1]/div[3]/div/div[1]/div[2]/div[2]/span')[0].text_content()
-    data["tow_bar"] = source.xpath('/html/body/div[2]/div/div[1]/div[2]/div[3]/div/div[2]/div[1]/div[2]/div/div[2]/div[3]/div[2]/span')[0].text_content()
-    data["fuel_consumption"] = source.xpath('/html/body/div[2]/div/div[1]/div[2]/div[3]/div/div[2]/div[1]/div[4]/div/div[1]/div[1]/div[2]/span[1]')[0].text_content()
-    data["cylinders"] = source.xpath('/html/body/div[2]/div/div[1]/div[2]/div[3]/div/div[2]/div[1]/div[3]/div/div[2]/div[3]/div[2]/span')[0].text_content()
-    data["plugin_hybrid"] = source.xpath('/html/body/div[2]/div/div[1]/div[2]/div[3]/div/div[2]/div[1]/div[3]/div/div[1]/div[3]/div[2]/span')[0].text_content()
-    data["electricity_consumption"] = source.xpath('/html/body/div[2]/div/div[1]/div[2]/div[3]/div/div[2]/div[1]/div[4]/div/div[1]/div[2]/div[2]/span[1]')[0].text_content()
-    data["electric_range"] = source.xpath('/html/body/div[2]/div/div[1]/div[2]/div[3]/div/div[2]/div[1]/div[4]/div/div[2]/div[2]/div[2]/span')[0].text_content()
-    data["battery_capacity"] = source.xpath('/html/body/div[2]/div/div[1]/div[2]/div[3]/div/div[2]/div[1]/div[4]/div/div[2]/div[3]/div[2]/span')[0].text_content()
-    data["body_type"] = source.xpath('/html/body/div[2]/div/div[1]/div[2]/div[3]/div/div[2]/div[1]/div[6]/div/div[1]/div[1]/div[2]/span')[0].text_content()
-    data["particle_filter"] = source.xpath('/html/body/div[2]/div/div[1]/div[2]/div[3]/div/div[2]/div[1]/div[8]/div/div[1]/fieldset/div[6]/div[2]/span')[0].text_content()
-    data["doors"] = source.xpath('/html/body/div[2]/div/div[1]/div[2]/div[3]/div/div[2]/div[1]/div[6]/div/div[1]/div[3]/div[2]/span')[0].text_content()
+    data["total_weight"] = get_value_from_xpath(source, XPATHS["page_2"]["total_weight"])
+    data["vehicle_weight"] = get_value_from_xpath(source, XPATHS["page_2"]["vehicle_weight"])
+    data["propulsion"] = get_value_from_xpath(source, XPATHS["page_2"]["propulsion"])
+    data["tow_bar"] = get_value_from_xpath(source, XPATHS["page_2"]["tow_bar"])
+    data["fuel_consumption"] = get_value_from_xpath(source, XPATHS["page_2"]["fuel_consumption"])
+    data["cylinders"] = get_value_from_xpath(source, XPATHS["page_2"]["cylinders"])
+    data["plugin_hybrid"] = get_value_from_xpath(source, XPATHS["page_2"]["plugin_hybrid"])
+    data["electricity_consumption"] = get_value_from_xpath(source, XPATHS["page_2"]["electricity_consumption"])
+    data["electric_range"] = get_value_from_xpath(source, XPATHS["page_2"]["electric_range"])
+    data["battery_capacity"] = get_value_from_xpath(source, XPATHS["page_2"]["battery_capacity"])
+    data["body_type"] = get_value_from_xpath(source, XPATHS["page_2"]["body_type"])
+    data["particle_filter"] = get_value_from_xpath(source, XPATHS["page_2"]["particle_filter"])
+    data["doors"] = get_value_from_xpath(source, XPATHS["page_2"]["doors"])
     return data
 
 # Page 3: "Syn"
@@ -39,8 +46,8 @@ def page_2(source):
 # Page 4: "Forsikring"
 def page_4(source):
     data = {"insurance": {}}
-    data["insurance"]["company"] = source.xpath('//*[@id="lblSelskab"]')[0].text_content()
-    data["insurance"]["is_active"] = True if source.xpath('//*[@id="lblStatus"]')[0].text_content() == "Aktiv" else False
-    data["insurance"]["number"] = None if source.xpath('//*[@id="lblBevisNummer"]')[0].text_content() == "N/A" else source.xpath('//*[@id="lblBevisNummer"]')[0].text_content()
-    data["insurance"]["created"] = source.xpath('//*[@id="lblbevisdato"]')[0].text_content()
+    data["insurance"]["company"] = get_value_from_xpath(source, XPATHS["page_4"]["company"])
+    data["insurance"]["is_active"] = True if get_value_from_xpath(source, XPATHS["page_4"]["is_active"]) == "Aktiv" else False
+    data["insurance"]["number"] = None if get_value_from_xpath(source, XPATHS["page_4"]["number"]) == "N/A" else get_value_from_xpath(source, XPATHS["page_4"]["number"])
+    data["insurance"]["created"] = get_value_from_xpath(source, XPATHS["page_4"]["created"])
     return data
