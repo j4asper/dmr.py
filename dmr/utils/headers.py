f4a420936ccf86146ec71a0db0a7796f75b3c141
Jasper <jasper@jazper.dk>
2023-03-17 10:48:04 +0100

    Remove unused header data

--- dmr/utils/headers.py
+++ dmr/utils/headers.py
@@ -2,18 +2,7 @@ from .user_agent import get_user_agent
 
 def get_headers(data:dict=None):
     headers = {
-        'Host': 'motorregister.skat.dk',
         'User-Agent': get_user_agent(),
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-        'Accept-Language': 'en-US,en;q=0.5',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'DNT': '1',
-        'Connection': 'keep-alive',
-        'Upgrade-Insecure-Requests': '1',
-        'Sec-Fetch-Dest': 'document',
-        'Sec-Fetch-Mode': 'navigate',
-        'Sec-Fetch-Site': 'none',
-        'Sec-Fetch-User': '?1',
     }
 
     if data is None:
