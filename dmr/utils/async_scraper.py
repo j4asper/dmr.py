14a0157243ff2adfc546d2f67b8a70f637f3f7e8
Jasper <jasper@jazper.dk>
2023-03-17 11:06:00 +0100

    Move get_token function out of scrape function

--- dmr/utils/async_scraper.py
+++ dmr/utils/async_scraper.py
@@ -5,8 +5,7 @@ from .extract_data import *
 from .errors import MissingToken
 from .xpaths import XPATHS
 
-async def scrape_async(license_plate:str):
-    async def get_token(session):
+async def get_token(session):
         """Get dmrFormToken"""
         async with session.get("https://motorregister.skat.dk/dmr-kerne/koeretoejdetaljer/visKoeretoej", headers=get_headers(), allow_redirects=True) as resp:
             content = await resp.text()
@@ -17,6 +16,8 @@ async def scrape_async(license_plate:str):
             return token, url
         except (TypeError, KeyError):
             raise MissingToken("The scraper wasn't able to get a token from motorregister.skat.dk, the site may have changed.")
+
+async def scrape_async(license_plate:str):
 
     async with ClientSession() as session:
         # Get dmrFormToken required to make site requests and get url to post data
@@ -40,14 +41,16 @@ async def scrape_async(license_plate:str):
         source = fromstring(content)
         data = page_1(source)
 
+        new_url_without_last_16_chars = new_url[:-16]
+
         # Page 2 scrape
-        async with session.get(str(resp.url) + "&_eventId=customPage&_pageIndex=1", headers=get_headers({"Referer":"https://motorregister.skat.dk" + new_url[:-16]}), allow_redirects=True) as resp:
+        async with session.get(str(resp.url) + "&_eventId=customPage&_pageIndex=1", headers=get_headers({"Referer":"https://motorregister.skat.dk" + new_url_without_last_16_chars}), allow_redirects=True) as resp:
             content = await resp.text()
         source = fromstring(content)
         data.update(page_2(source))
 
         # Page 4 scrape
-        async with session.get(str(resp.url) + "&_eventId=customPage&_pageIndex=3", headers=get_headers({"Referer":"https://motorregister.skat.dk" + new_url[:-16]}), allow_redirects=True) as resp:
+        async with session.get(str(resp.url) + "&_eventId=customPage&_pageIndex=3", headers=get_headers({"Referer":"https://motorregister.skat.dk" + new_url_without_last_16_chars}), allow_redirects=True) as resp:
             content = await resp.text()
         source = fromstring(content)
         data.update(page_4(source))
--- dmr/utils/scraper.py
+++ dmr/utils/scraper.py
@@ -4,18 +4,18 @@ from .headers import get_headers
 from .extract_data import *
 from .errors import MissingToken
 
+def get_token(session):
+    """Get dmrFormToken"""
+    resp = session.get("https://motorregister.skat.dk/dmr-kerne/koeretoejdetaljer/visKoeretoej", headers=get_headers(), allow_redirects=True)
+    source = fromstring(resp.text)
+    try:
+        token = source.xpath(XPATHS["other"]["token"])[0].get("value")
+        url = source.xpath(XPATHS["other"]["token_url"])[0].get("action")
+        return token, url
+    except (TypeError, KeyError):
+        raise MissingToken("The scraper wasn't able to get a token from motorregister.skat.dk, the site may have changed.")
 
 def scrape(license_plate:str):
-    def get_token(session):
-        """Get dmrFormToken"""
-        resp = session.get("https://motorregister.skat.dk/dmr-kerne/koeretoejdetaljer/visKoeretoej", headers=get_headers(), allow_redirects=True)
-        source = fromstring(resp.text)
-        try:
-            token = source.xpath(XPATHS["other"]["token"])[0].get("value")
-            url = source.xpath(XPATHS["other"]["token_url"])[0].get("action")
-            return token, url
-        except (TypeError, KeyError):
-            raise MissingToken("The scraper wasn't able to get a token from motorregister.skat.dk, the site may have changed.")
 
     with Session() as session:
         # Get dmrFormToken required to make site requests and get url to post data
