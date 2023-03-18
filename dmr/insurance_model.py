861640295547fb6119f9dc59d6808407b8e7045f
Jasper <jasper@jazper.dk>
2023-03-18 12:40:38 +0100

    Add Insurance class to seperate file

---
+++ dmr/insurance_model.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+from pydantic import BaseModel
+from typing import Optional
+import datetime
+
+class Insurance(BaseModel):
+    """
+    The Insurance object holds the insurance information about the vehicle.
+
+    Possible attributes:
+
+        Insurance.company (str): The insurance provider.
+
+        Insurance.is_active (bool): Whether or not the insurance is active.
+
+        Insurance.number (str): The insurance number, typically returns None.
+
+        Insurance.created (datetime): The date when the insurance was created.
+    """
+    company: str
+    is_active: bool
+    number: Optional[int]
+    created: datetime.datetime
