e8fe72b4775ea7ec99f7a7ff3b3ddd8b6ee195aa
Jasper <jasper@jazper.dk>
2023-09-07 13:19:41 +0200

    Add docstring

--- dmr/models/vehicle.py
+++ dmr/models/vehicle.py
@@ -18,28 +18,29 @@ class Vehicle(BaseModel):
         make:                      The make/manufacturer of the vehicle
         model:                     The vehicle model, if the vehicle were to be a Suzuki Swift, then the model would be "Swift\"
         variant:                   This is the variant of the model, a VW Golf GTI, would have the GTI variation of the Golf model
-        type:
-        color:
-        total_weight:
-        vin:
-        last_update:
-        registration_number:
-        first_registration:
-        use:
-        model_year:
-        vehicle_weight:
-        propulsion:
-        tow_bar:
-        fuel_consumption:
-        cylinders:
-        plugin_hybrid:
-        electricity_consumption:
-        electric_range:
-        battery_capacity:
-        body_type:
-        vehicle_id:
-        doors:
-        particle_filter:
+        type:                      The type of vehicle
+        color:                     The color of the vehicle, typically None
+        total_weight:              The maximum weight the vehicle can be when fully loaded, value is in KG
+        vin:                       The vehicles identification number (VIN)
+        last_update:               The last vehicle update, this could be the latest re-registration
+        registration_number:       The vehicles license plate
+        first_registration:        The date where the vehicle was first registered
+        use:                       The type of usage the vehicle is being used for
+        model_year:                The model year of the vehicle
+        vehicle_weight:            The weight of the vehicle. The value returned is in KG
+        propulsion:                The type fuel needed to use the vehicle
+        tow_bar:                   Whether the vehicle has a tow bar
+        fuel_consumption:          The fuel consumption of the vehicle, the value can be km/L if the vehicle runs on Benzin or Diesel
+        cylinders:                 The amount of cylinders the vehicle has. A return value of None doesn't necessarily mean that the vehicle has no cylinders
+        plugin_hybrid:             Whether the vehicle is a Plug-In hybrid
+        electricity_consumption:   The electric consumption of the vehicle, the value can be Wh/km
+        electric_range:            The electric range of the vehicle, value is in km
+        battery_capacity:          The battery capacity of the vehicle, value is in kWh
+        body_type:                 The body type of the vehicle
+        vehicle_id:                The vehicle ID in the DMR system
+        doors:                     The amount of doors the vehicle has
+        particle_filter:           Whether the vehicle has a particle filter
+        insurance:                 An insurance object
     """
     make: str
     model: str
@@ -62,7 +63,7 @@ class Vehicle(BaseModel):
     electricity_consumption: Optional[float]
     electric_range: Optional[float]
     battery_capacity: Optional[float]
-    body_type: Optional[BodyType]  # ENUM ???
+    body_type: Optional[BodyType]
     vehicle_id: int
     doors: Optional[int]
     particle_filter: Optional[bool]
