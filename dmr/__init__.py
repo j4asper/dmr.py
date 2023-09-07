96addfad9a10a65506718197333bd7e903222f0e
Jasper <jasper@jazper.dk>
2023-09-07 12:31:16 +0200

    Add VehicleType and BodyType import

--- dmr/__init__.py
+++ dmr/__init__.py
@@ -1,4 +1,6 @@
 from .models.insurance import Insurance
 from .models.vehicle import Vehicle
 from .models.propulsion_type import PropulsionType
+from .models.vehicle_type import VehicleType
+from .models.body_type import BodyType
 from .models.dmr import DMR
