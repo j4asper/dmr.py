a750d80e285bc768ec5f8900dac815986d2b2a63
Jasper <jasper@jazper.dk>
2023-09-07 13:10:00 +0200

    Add UseType import

--- dmr/__init__.py
+++ dmr/__init__.py
@@ -3,4 +3,5 @@ from .models.vehicle import Vehicle
 from .models.propulsion_type import PropulsionType
 from .models.vehicle_type import VehicleType
 from .models.body_type import BodyType
+from .models.use_type import UseType
 from .models.dmr import DMR
