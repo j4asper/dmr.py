57d4831b37ce5f216646872af66bb20ad5dcda40
Jasper <jasper@jazper.dk>
2023-09-07 12:31:03 +0200

    Add docstring

--- dmr/models/propulsion_type.py
+++ dmr/models/propulsion_type.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 
 class PropulsionType(str, Enum):
+    """Vehicle propulsion type, the kind of fuel the car uses"""
     diesel = "Diesel"
     benzin = "Benzin"
     electric = "El"
