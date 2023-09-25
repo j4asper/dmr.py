717f01c55d0e33200335f88a13c075ab655a00b9
Jasper <jasper@jazper.dk>
2023-09-25 15:05:15 +0200

    Add __init__ for model classes

---
+++ dmr/models/__init__.py
@@ -0,0 +1,6 @@
+from .body_type import BodyType
+from .insurance import Insurance
+from .propulsion_type import PropulsionType
+from .use_type import UseType
+from .vehicle_type import VehicleType
+from .vehicle import Vehicle
