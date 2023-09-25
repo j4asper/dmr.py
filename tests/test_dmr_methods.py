6eeab41b3a8cc8ff20b6b0c05f645a7209692ce7
Jasper <jasper@jazper.dk>
2023-09-25 15:19:44 +0200

    Add invalid plate test async

--- tests/test_dmr_methods.py
+++ tests/test_dmr_methods.py
@@ -1,4 +1,5 @@
 from dmr import DMR
+import asyncio
 
 
 def test_validate_license_plate():
@@ -12,4 +13,9 @@ def test_validate_license_plate():
 
 def test_invalid_license_plate():
     dmr_obj = DMR.get_by_plate(license_plate="GGGGGGG")
+    assert dmr_obj is None
+
+def test_invalid_license_plate_async():
+    loop = asyncio.new_event_loop()
+    dmr_obj = loop.run_until_complete(DMR.get_by_plate_async("GGGGGGG"))
     assert dmr_obj is None
